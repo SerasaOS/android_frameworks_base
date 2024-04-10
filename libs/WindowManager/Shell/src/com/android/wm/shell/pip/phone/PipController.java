b9ca9cd322a0de8ba5d7ae0389815828712c4418
pioush garg <pioushkumar1992@gmail.com>
2024-04-10 14:58:09 +0530

    Crash in SystemUI in Multi-User case for PIP.
    Description:
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: Process: com.android.systemui, PID: 10252
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: java.lang.IllegalStateException: Existing input consumer found with name: pip_input_consumer, display: 0
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: 	at android.os.Parcel.createExceptionOrNull(Parcel.java:2382)
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: 	at android.os.Parcel.createException(Parcel.java:2358)
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:2341)
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:2283)
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: 	at android.view.IWindowManager$Stub$Proxy.createInputConsumer(IWindowManager.java:5175)
    04-10 13:50:21.197 1010145 10252 10252 E AndroidRuntime: 	at com.android.systemui.shared.system.InputConsumerController.registerInputConsumer(InputConsumerController.java:165)
    
    Crash is occurring. If two users are present. Open any app in PIP. Then keep on moving it to PIP to non-PIP and non-PIP to PIP.
    Eventually crash will happen because onActivity Pinned is getting called 2 times. pip_input_consumer that is already present, is getting created again.
    
    Fix: Do nothing if user is different.
    
    Change-Id: Ie49d7ed525b5c5583c96f54ed3fb0901ff76c6ec
    Signed-off-by: pioush garg <pioushkumar1992@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/WindowManager/Shell/src/com/android/wm/shell/pip/phone/PipController.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/pip/phone/PipController.java
@@ -48,6 +48,7 @@ import android.graphics.Rect;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.util.Pair;
 import android.view.DisplayInfo;
 import android.view.InsetsState;
@@ -559,6 +560,8 @@ public class PipController implements PipTransitionController.PipTransitionCallb
                     @Override
                     public void onActivityPinned(String packageName, int userId, int taskId,
                             int stackId) {
+                        if (userId != UserHandle.myUserId()) return;
+
                         ProtoLog.d(ShellProtoLogGroup.WM_SHELL_PICTURE_IN_PICTURE,
                                 "onActivityPinned: %s", packageName);
                         mTouchHandler.onActivityPinned();
